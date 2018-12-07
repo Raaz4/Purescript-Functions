9a97c68cb0f2bcef7ea6068fb3608b1a3f942545
Raaz4 <34880700+Raaz4@users.noreply.github.com>
2018-12-07 19:29:30 +0530

    Update Main.purs

--- Control.Applicative/src/Main.purs
+++ Control.Applicative/src/Main.purs
@@ -10,8 +10,8 @@ quot a b = a / b
 
 array = [1,2,5,4,6,7,4,9,8,5,3,8]
 
-apply1 :: forall a b. f (a -> b) -> f a -> f b
-apply1 a b = apply a b
+-- apply1 :: forall a b. f (a -> b) -> f a -> f b
+-- apply1 a b = apply a b
 
 main :: Effect Unit
 main = do
