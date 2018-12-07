11b0116fc35827591b5717c27d405e6121bf5de8
raz9 <razeen.shaikh@juspay.in>
2018-12-07 20:01:50 +0530

    Addedd some new functions

--- Control.Applicative/src/Main.purs
+++ Control.Applicative/src/Main.purs
@@ -4,16 +4,45 @@ import Prelude
 import Effect (Effect)
 import Effect.Class.Console (logShow)
 import Control.Applicative
+import Math
+import Data.Maybe
 
 quot :: Int -> Int -> Int
 quot a b = a / b
 
-array = [1,2,5,4,6,7,4,9,8,5,3,8]
+ad a = a + 5
+sub a = 5 - a
 
-apply1 :: forall a b. f (a -> b) -> f a -> f b
+arraynum =  [1.2,43.43,3.42,3.42,42.2,23.23]
+
+arrayint = [1,2,5,4,6,7,4,9,8,5,3,8]
+
+apply1 :: forall f a b. Apply f => f (a -> b) -> f a -> f b
 apply1 a b = apply a b
+
+liftA11 :: forall f a b. Applicative f => (a -> b) -> f a -> f b
+liftA11 f array = liftA1 f array
+
+unless1 :: forall m. Applicative m => Boolean -> m Unit -> m Unit
+unless1 b u = unless b u
+
+when1 :: forall m. Applicative m => Boolean -> m Unit -> m Unit
+when1 b u = when b u
 
 main :: Effect Unit
 main = do
-  logShow (quot <$> array <*> array)
-  -- logShow (apply1 (quot <$> array) array)
+  logShow (apply1 [ad, sub] arrayint)
+  logShow (liftA11 ceil arraynum)
+  logShow (unless1 true (Just unit))
+  logShow (when1 false (Just unit))
+
+  --  Operators
+  
+  logShow (quot <$> arrayint <*> arrayint)
+  logShow ([ad, sub] <*> arrayint)
+  logShow (arrayint <* [ad, sub])
+  logShow ([ad, sub] *> arrayint)
+  logShow (ad <$> arrayint)
+  logShow (3 <$ arrayint)
+  logShow (arrayint $> 6)
+  logShow (arrayint <#> ad)
